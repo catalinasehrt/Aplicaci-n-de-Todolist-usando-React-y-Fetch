8c554c21dbb9f798eb3011da262a3f98ae99e4ce
catalinasehrt <catalina.dali94@gmail.com>
2023-02-22 15:18:49 +0000

    empece

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import React, {useState, useEffect} from "react";
+
+export default function App () {}
+return (
+    <div className="App">
+        <h1>Todolist fetch</h1>
+        <form onSubmit={()=>{
+            alert ("se envia una tarea")
+            
+
+        }}></form>
+
+
+    </div>
+    
+)
